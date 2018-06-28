88977ab356f8821d98e2bc537ab70a3b76926d14
Luis Felipe Kaufmann da Silva <luis.kaufmann@iphytus.com>
2018-06-28 16:08:54 -0300

    a99

--- mymoneyapp/backend/config/server.js
+++ mymoneyapp/backend/config/server.js
@@ -4,10 +4,12 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const server = express();
 const cors = require('./cors');
+const expressQueryInt = require("express-query-int");
 
 server.use(bodyParser.urlencoded({extended: true}));
 server.use(bodyParser.json());
 server.use(cors);
+server.use(expressQueryInt());
 
 server.listen(PORT, function () {
     console.log(`[BACKEND] is listen: ${PORT}`);
